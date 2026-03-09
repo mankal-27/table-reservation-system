0208e5107d8408a5582024626fdc98a071b9346b
mankal-27 <mkalkutagi27@gmail.com>
2026-03-09 16:02:44 +0530

    new docker

--- booking-service/config/rabbitmq.js
+++ booking-service/config/rabbitmq.js
@@ -1,22 +1,28 @@
 const amqp = require('amqplib');
+const RABBITMQ_URL = process.env.RABBITMQ_URL || 'amqp://localhost:5672';
 
 let channel = null;
 
 const connectRabbitMQ = async () => {
-  try {
-    // Connect to the RabbitMQ Docker container running on localhost:5672
-    const connection = await amqp.connect('amqp://localhost:5672');
-    channel = await connection.createChannel();
-    
-    // Ensure the queue exists before we try to send messages to it
-    await channel.assertQueue('booking_events', { durable: true });
-    
-    console.log('🐇 Connected to RabbitMQ & Queue Ready');
-  } catch (error) {
-    console.error('❌ RabbitMQ Connection Failed:', error);
+  let retries = 5;
+  
+  while (retries > 0) {
+    try {
+      const connection = await amqp.connect(RABBITMQ_URL);
+      channel = await connection.createChannel();
+      await channel.assertQueue('booking_events', { durable: true });
+      
+      console.log('🐇 Connected to RabbitMQ & Queue Ready');
+      break; // Success! Exit the loop.
+      
+    } catch (error) {
+      console.log(`⏳ RabbitMQ not ready yet. Retrying in 5 seconds... (${retries} attempts left)`);
+      retries -= 1;
+      // Wait for 5 seconds before the loop runs again
+      await new Promise(res => setTimeout(res, 5000)); 
+    }
   }
 };
 
 const getChannel = () => channel;
-
 module.exports = { connectRabbitMQ, getChannel };
